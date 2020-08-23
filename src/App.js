aec087cb16c0aa3d09c8b7fb9212989b28b845d7
ruso11 <aok95@hotmail.com.ar>
2020-08-23 20:52:08 -0300

    se agregó plantilla

--- src/App.js
+++ src/App.js
@@ -1,11 +1,72 @@
 import React from 'react';
 import './App.css';
-import { Button } from 'antd';
+import { Button, Radio } from 'antd';
+import { Layout, Menu } from 'antd';
+import {
+  AppstoreOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  ShopOutlined,
+  TeamOutlined,
+  UserOutlined,
+  UploadOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
 
-const App = () => (
-  <div className="App">
-    <Button type="primary">Button</Button>
-  </div>
-);
+const App = () => {
+
+  const { Header, Content, Footer, Sider } = Layout;
+
+  return(
+    <Layout>
+    <Sider
+      style={{
+        overflow: 'auto',
+        height: '100vh',
+        position: 'fixed',
+        left: 0,
+      }}
+    >
+      <div className="logo" />
+      <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+        <Menu.Item key="1" icon={<UserOutlined />}>
+          nav 1
+        </Menu.Item>
+        <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+          nav 2
+        </Menu.Item>
+        <Menu.Item key="3" icon={<UploadOutlined />}>
+          nav 3
+        </Menu.Item>
+        <Menu.Item key="4" icon={<BarChartOutlined />}>
+          nav 4
+        </Menu.Item>
+        <Menu.Item key="5" icon={<CloudOutlined />}>
+          nav 5
+        </Menu.Item>
+        <Menu.Item key="6" icon={<AppstoreOutlined />}>
+          nav 6
+        </Menu.Item>
+        <Menu.Item key="7" icon={<TeamOutlined />}>
+          nav 7
+        </Menu.Item>
+        <Menu.Item key="8" icon={<ShopOutlined />}>
+          nav 8
+        </Menu.Item>
+      </Menu>
+    </Sider>
+    <Layout className="site-layout" style={{ marginLeft: 200 }}>
+      <Header className="site-layout-background" style={{ padding: 0 }} />
+      <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+        <div className="site-layout-background" style={{ padding: 24, textAlign: 'center' }}>
+        <h2>asdsadsad</h2>
+
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
+  </Layout>  
+  )
+}
 
 export default App;
